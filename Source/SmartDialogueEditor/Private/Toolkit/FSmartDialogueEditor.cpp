0a61a2e81edde7a59ecce9d6dc1b69d215fced5b
Nikolay Unru <n.unru@demagicgames.com>
2023-03-21 21:52:43 +0600

    Editor toolkit in progress

--- Source/SmartDialogueEditor/Private/Toolkit/FSmartDialogueEditor.cpp
+++ Source/SmartDialogueEditor/Private/Toolkit/FSmartDialogueEditor.cpp
@@ -2,3 +2,39 @@
 
 
 #include "FSmartDialogueEditor.h"
+
+#include "SmartDialogue.h"
+
+#define LOCTEXT_NAMESPACE "SmartDialogueEditor"
+
+FName FSmartDialogueEditor::GetToolkitFName() const
+{
+	return FName("SmartDialogueEditor");
+}
+
+FText FSmartDialogueEditor::GetToolkitName() const
+{
+	const bool bDirtyState = EditingObject->GetOutermost()->IsDirty();
+
+	FFormatNamedArguments Args;
+	Args.Add(TEXT("AssetName"), FText::FromString(EditingObject->GetName()));
+	Args.Add(TEXT("DirtyState"), bDirtyState ? FText::FromString( TEXT( "*" ) ) : FText::GetEmpty());
+	return FText::Format(LOCTEXT("CustomAssetTypeEditorAppLabel", "{SpriteName}{DirtyState}"), Args);
+}
+
+FText FSmartDialogueEditor::GetBaseToolkitName() const
+{
+	return LOCTEXT("SmartDialogueEditorAppLabel", "Smart Dialogue Editor");
+}
+
+FString FSmartDialogueEditor::GetWorldCentricTabPrefix() const
+{
+	return TEXT("SmartDialogueEditor");
+}
+
+FLinearColor FSmartDialogueEditor::GetWorldCentricTabColorScale() const
+{
+	return FLinearColor::White;
+}
+
+#undef LOCTEXT_NAMESPACE
--- Source/SmartDialogueEditor/Private/Toolkit/FSmartDialogueEditor.h
+++ Source/SmartDialogueEditor/Private/Toolkit/FSmartDialogueEditor.h
@@ -5,7 +5,17 @@
 #include "CoreMinimal.h"
 #include "Toolkits/AssetEditorToolkit.h"
 
-class SMARTDIALOGUEEDITOR_API FSmartDialogueEditor : public FAssetEditorToolkit
+class USmartDialogue;
+
+class SMARTDIALOGUEEDITOR_API FSmartDialogueEditor final : public FAssetEditorToolkit
 {
-	
+public:
+	virtual FName GetToolkitFName() const override;
+	virtual FText GetToolkitName() const override;
+	virtual FText GetBaseToolkitName() const override;
+	virtual FString GetWorldCentricTabPrefix() const override;
+	virtual FLinearColor GetWorldCentricTabColorScale() const override;
+
+private:
+	USmartDialogue* EditingObject;
 };
