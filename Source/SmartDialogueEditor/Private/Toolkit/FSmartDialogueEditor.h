e2518eb0369764cda3391201887b2015aad3ed36
Nikolay Unru <n.unru@demagicgames.com>
2023-03-21 21:43:09 +0600

    Added empty FSmartDialogueEditor

---
+++ Source/SmartDialogueEditor/Private/Toolkit/FSmartDialogueEditor.h
@@ -0,0 +1,11 @@
+// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Toolkits/AssetEditorToolkit.h"
+
+class SMARTDIALOGUEEDITOR_API FSmartDialogueEditor : public FAssetEditorToolkit
+{
+	
+};
